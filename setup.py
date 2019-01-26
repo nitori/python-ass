9152843b6be321df4dbfb656f9f310d73af594ee
FichteFoll <fichtefoll2@googlemail.com>
2019-01-26 01:00:03 +0100

    Update meta info in preparation for new pypi pkg

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name = 'ass',
-    version = '0.4.2',
+    name='ass',
+    version='0.4.3',
     description='A library for parsing and manipulating Advanced SubStation Alpha subtitle files.',
     author='Tony Young',
-    author_email='tony@rfw.name',
+    author_email='python-ass@chireiden.net',
     keywords='ass subtitle substation alpha',
-    packages = ['ass'],
-    url = 'http://github.com/rfw/python-ass',
-    license = 'MIT',
-    classifiers = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3',
-    'Topic :: Multimedia :: Video',
-    'Topic :: Software Development :: Libraries',
-    'Topic :: Text Processing :: Markup'],
-    install_requires = ['setuptools'],
-    zip_safe=True)
+    packages=['ass'],
+    url='http://github.com/chireiden/python-ass',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Text Processing :: Markup',
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+)
