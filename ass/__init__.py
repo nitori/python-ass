628b2b9e00f59b07bdabca9d9bb5cfbfbc0b5905
FichteFoll <fichtefoll2@googlemail.com>
2019-05-03 18:19:49 +0200

    Don't load renderer by default (and emit a Warning)
    Closes #4.

--- ass/__init__.py
+++ ass/__init__.py
@@ -1,11 +1,6 @@
 from . import document
 
-try:
-    from . import renderer
-except Exception as e:
-    import warnings
-    warnings.warn("Could not load renderer: " + str(e))
 
-__all__ = ["document", "renderer", "parse"]
+__all__ = ["document", "parse"]
 
 parse = document.Document.parse_file
